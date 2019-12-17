3545608f75705fd7c3311709f83d600f7afe835b
Daniel Sun <danieljsun@gmail.com>
2019-12-17 17:21:58 +1100

    chapter 2 - believe in types

---
+++ type.hs
@@ -0,0 +1,11 @@
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+anInt :: Int
+anInt = 1
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
